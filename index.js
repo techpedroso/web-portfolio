0c3f352716191d32721f3f61754711320c80df18
techpedroso <technologypedroso@gmail.com>
2026-02-04 00:54:00 -0300

    Add menu mobile first version

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import changeTheme from "./js/theme.js";
 
+const menuBtn = document.querySelector('.mobile-menu-btn');
+const navContainer = document.querySelector('.nav-container');
+
+menuBtn.addEventListener('click', () => {
+    navContainer.classList.toggle('active');
+    
+    // Opcional: Animação básica do hambúrguer virando "X"
+    menuBtn.classList.toggle('open');
+});
+
+// Fechar o menu ao clicar em um link
+document.querySelectorAll('.nav-container a').forEach(link => {
+    link.addEventListener('click', () => {
+        navContainer.classList.remove('active');
+    });
+});
+
 changeTheme();
